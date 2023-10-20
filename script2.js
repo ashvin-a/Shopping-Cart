2158949e0fc9c62b4f42ba4cf017b949c8092643
Ashvin <ashvin.anilkumar@qburst.com>
2023-10-20 16:54:07 +0530

    initial steps for filtering process

--- script2.js
+++ script2.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.item-grid');
-const filter = document.querySelectorAll('.sm-filter')
+const filter = document.querySelector('.filter')
+
 async function getData() {
     const res = await fetch('./items.json');
     const datas = await res.json();
@@ -9,12 +10,16 @@ async function getData() {
         const div2 = document.createElement('div');
         const description = document.createElement('p');
         description.style.textAlign = 'justify';
-        description.innerHTML = `<span class=name>${data.name}</span>
-        &nbsp <span class=price>${data.price}</span><br>
-        ${data.description}`;
-        div.appendChild(div2);
         div.className = data.class;
         div2.className = data.class;
+        if(true===true){
+            console.log('workin')
+        }
+        description.innerHTML = `<span class=name>${data.name}</span>
+        &nbsp&nbsp<span class=price>${data.price}</span><br>
+        ${data.description}`;
+        div.appendChild(div2);
+        div.style.boxShadow = '10px 1px 10px 10px rgba(166, 166, 166, 0.703)'
         div.classList.add('item');
         div2.innerHTML = `<img src="images/${data.image}" alt="" />`;
         if (
@@ -29,9 +34,13 @@ async function getData() {
     });
 }
 
+function filterIt(e){
+    console.log(e.target.classList)//this is it bro we got this
+}
+
 function init() {
     document.addEventListener('DOMContentLoaded', getData);
-    document.addEventListener();
+    filter.addEventListener('click',filterIt);
 }
 
 init();
